c8d75df4af9babcb85497a256feb1b6b178c47d3
August Hedman <augustlh@icloud.com>
2024-12-01 18:42:40 +0100

    day1-part1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let mut i: Vec<i32> = Vec::new();
+    let mut j: Vec<i32> = Vec::new();
+
+    for line in read_to_string("src/day1.txt").unwrap().lines() {
+        let line_contents: Vec<&str> = line.split_whitespace().collect();
+    
+        i.push(line_contents[0].parse::<i32>().expect("Failed to parse"));
+        j.push(line_contents[1].parse::<i32>().expect("Failed to parse"));
+    }
+
+    
+    i.sort();
+    j.sort();
+
+    let mut res: i32 = 0;
+
+    for idx in 0..i.len() {
+        res += (i[idx] - j[idx]).abs();
+    }
+
+    println!("{:?}", res);
 }
